4321562b5f69e349595819f615b5ffaed05fda40
tiramify <tiramify@gmail.com>
2024-01-26 01:03:38 +0100

    let's go jit!

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,14 @@ const Value = u64;
 const VMRegister = usize;
 const VMLocal = usize;
 
+const LessThan = struct {
+    lhs: VMRegister,
+};
+const JumpConditional = struct {
+    true_target: *Block,
+    false_target: *Block,
+};
+
 const Instruction = union(enum) {
     LoadImmediate: Value,
     Load: VMRegister,
@@ -19,12 +27,10 @@ const Instruction = union(enum) {
     SetLocal: VMLocal,
     GetLocal: VMLocal,
     Increment: void,
-    LessThan: struct { lhs: VMRegister }, // lhs
+    LessThan: LessThan, // lhs
     Jump: *Block,
-    JumpConditional: struct {
-        true_target: *Block,
-        false_target: *Block,
-    },
+    JumpConditional: JumpConditional,
+    Exit: void,
 
     pub const init = UnionInit(@This());
 
@@ -46,7 +52,7 @@ const VM = struct {
         var vm: VM = .{
             .allocator = allocator,
             .registers = try Registers.initCapacity(allocator, 8),
-            .locals = try Locals.initCapacity(allocator, 24),
+            .locals = try Locals.initCapacity(allocator, 12),
         };
         vm.registers.expandToCapacity();
         vm.locals.expandToCapacity();
@@ -63,22 +69,46 @@ const VM = struct {
         self.registers.deinit();
         self.locals.deinit();
     }
+
+    pub fn dump(self: *@This(), writer: anytype) !void {
+        try writer.print("Registers {d}:\n", .{self.registers.items.len});
+        for (self.registers.items, 0..) |reg, i| {
+            try writer.writeByteNTimes(' ', 4);
+            try writer.print("{d}: {d}\n", .{ i, reg });
+        }
+
+        try writer.print("Locals {d}:\n", .{self.locals.items.len});
+        for (self.locals.items, 0..) |loc, i| {
+            try writer.writeByteNTimes(' ', 4);
+            try writer.print("{d}: {d}\n", .{ i, loc });
+        }
+
+        _ = try writer.write("\n");
+    }
 };
 
 const Block = struct {
     const Instructions = std.ArrayList(Instruction);
+    const Jumps = std.ArrayList(usize);
+
     instructions: Instructions,
+    // Offsets into the instruction stream where we have RIP-relative jump offsets to here that need patching.
+    jumps: Jumps,
+    // Offset where this block starts
+    offset: usize = 0,
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) @This() {
         return .{
             .instructions = Instructions.init(allocator),
+            .jumps = Jumps.init(allocator),
             .allocator = allocator,
         };
     }
 
     pub fn deinit(self: *@This()) void {
         self.instructions.deinit();
+        self.jumps.deinit();
     }
 
     pub fn dump(self: *@This(), writer: anytype, spaces: usize) !void {
@@ -165,6 +195,8 @@ pub fn main() !void {
     try block1.appendInit(.Store, 6);
     try block1.appendInit(.Jump, block4);
 
+    try block2.appendInit(.Exit, void{});
+
     try block3.appendInit(.LoadImmediate, 0);
     try block3.appendInit(.Jump, block5);
 
@@ -188,52 +220,331 @@ pub fn main() !void {
 
     try program.dump(std.io.getStdOut().writer());
 
-    var current_block = block1;
-    var ic: usize = 0; // Instruction Counter
-
     var vm = try VM.init(allocator);
     defer vm.deinit();
 
-    o: while (true) {
-        if (ic >= current_block.instructions.items.len) {
-            break :o;
-        }
+    var jit = JIT.init(allocator);
+    defer jit.deinit();
 
-        const instruction = current_block.instructions.items[ic];
+    var executable = try jit.compile(&program);
+    try executable.run(&vm);
 
-        switch (instruction) {
-            .LoadImmediate => |li| {
-                vm.registers.items[0] = li;
-            },
-            .Load => |l| {
-                vm.registers.items[0] = vm.registers.items[l];
-            },
-            .Store => |s| {
-                vm.registers.items[s] = vm.registers.items[0];
-            },
-            .SetLocal => |sl| vm.locals.items[sl] = vm.registers.items[0], //try vm.setLocal(sl, vm.registers.items[0]),
-            .GetLocal => |gl| vm.registers.items[0] = vm.locals.items[gl],
-            .Increment => {
-                vm.registers.items[0] += 1;
-            },
-            .LessThan => |lt| {
-                vm.registers.items[0] = if (vm.registers.items[lt.lhs] < vm.registers.items[0]) 1 else 0;
-            },
-            .Jump => |jmp| {
-                current_block = jmp;
-                ic = 0;
-                continue :o;
-            },
-            .JumpConditional => |jmpc| {
-                current_block = if (vm.registers.items[0] == 1)
-                    jmpc.true_target
-                else
-                    jmpc.false_target;
-                ic = 0;
-                continue :o;
-            },
-        }
-
-        ic += 1;
-    }
+    try vm.dump(std.io.getStdOut().writer());
 }
+
+const Executable = struct {
+    code: []u8,
+    code_size: usize,
+
+    pub const JITFunction = *const fn (usize, []Value, locals: []Value) void;
+
+    pub fn run(self: *@This(), vm: *VM) !void {
+        // RDI: VM&
+        // RSI: Value* registers
+        // RDX: Value* locals
+        // const func: JITFunction = @ptrCast(@alignCast(self.code));
+        //  func(0, vm.registers.items.ptr, vm.locals.items.ptr);
+
+        asm volatile ("call *%%rcx"
+            :
+            : [unholyRegion] "{rcx}" (self.code.ptr),
+              [a] "{rsi}" (vm.registers.items.ptr),
+              [b] "{rdx}" (vm.locals.items.ptr),
+        );
+    }
+};
+
+const Assembler = struct {
+    output: std.ArrayList(u8),
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) @This() {
+        return .{
+            .output = std.ArrayList(u8).init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.output.deinit();
+    }
+
+    const RegType = u8;
+    pub const Reg = enum(RegType) {
+        GPR0 = 0, // RAX
+        GPR1 = 1, // RCX
+
+        RegisterArrayBase = 6, // RSI
+        LocalsArrayBase = 2, // RDX
+
+        pub fn to_underlying(self: @This()) RegType {
+            return @intFromEnum(self);
+        }
+    };
+
+    const Operand = union(enum) {
+        reg: Reg,
+        imm64: Value,
+        mem_64_base_and_offset: struct {
+            base: Reg,
+            offset: u32,
+        },
+
+        pub const init = UnionInit(@This());
+    };
+
+    const deadbeef = 0xdeadbeef;
+
+    pub fn mov(self: *@This(), dst: Operand, src: Operand) !void {
+        if (dst == .reg and src == .reg) {
+            try self.emit8(0x48);
+            try self.emit8(0x89);
+            try self.emit8(0xc0 | (dst.reg.to_underlying() << 3) | src.reg.to_underlying());
+            return;
+        }
+
+        if (dst == .reg and src == .imm64) {
+            try self.emit8(0x48);
+            try self.emit8(0xb8 | dst.reg.to_underlying());
+            try self.emit64(src.imm64);
+            return;
+        }
+
+        if (dst == .mem_64_base_and_offset and src == .reg) {
+            try self.emit8(0x48);
+            try self.emit8(0x89);
+            try self.emit8(0x80 | (src.reg.to_underlying() << 3) | dst.mem_64_base_and_offset.base.to_underlying());
+            try self.emit32(dst.mem_64_base_and_offset.offset);
+            return;
+        }
+
+        if (dst == .reg and src == .mem_64_base_and_offset) {
+            // MOV dst, QWORD [src.base + src.offset]
+            try self.emit8(0x48);
+            try self.emit8(0x8b);
+            try self.emit8(0x80 | (dst.reg.to_underlying() << 3) | src.mem_64_base_and_offset.base.to_underlying());
+            try self.emit32(src.mem_64_base_and_offset.offset);
+            return;
+        }
+
+        unreachable;
+    }
+
+    pub fn emit8(self: *@This(), val: u8) !void {
+        try self.output.append(val);
+    }
+
+    pub fn emit32(self: *@This(), val: u32) !void {
+        try self.output.writer().writeInt(u32, val, .little);
+    }
+
+    pub fn emit64(self: *@This(), val: u64) !void {
+        try self.output.writer().writeInt(u64, val, .little);
+    }
+
+    pub fn emit(self: *@This(), comptime T: type, val: T) !void {
+        try self.output.writer().writeInt(T, val, .little);
+    }
+
+    // Instructions
+    pub fn load_immediate64(self: *@This(), dst: Reg, imm: Value) !void {
+        try self.mov(Operand.init(.reg, dst), Operand.init(.imm64, imm));
+    }
+
+    pub fn store_vm_register(self: *@This(), dst: VMRegister, src: Reg) !void {
+        try self.mov(Operand.init(.mem_64_base_and_offset, .{
+            .base = Reg.RegisterArrayBase,
+            .offset = @as(u32, @intCast(dst * @sizeOf(Value))),
+        }), Operand.init(.reg, src));
+    }
+
+    pub fn load_vm_register(self: *@This(), dst: Reg, src: VMRegister) !void {
+        try self.mov(Operand.init(.reg, dst), Operand.init(.mem_64_base_and_offset, .{
+            .base = Reg.RegisterArrayBase,
+            .offset = @as(u32, @intCast(src * @sizeOf(Value))),
+        }));
+    }
+
+    pub fn store_vm_local(self: *@This(), dst: VMLocal, src: Reg) !void {
+        try self.mov(Operand.init(.mem_64_base_and_offset, .{
+            .base = Reg.LocalsArrayBase,
+            .offset = @as(u32, @intCast(dst * @sizeOf(Value))),
+        }), Operand.init(.reg, src));
+    }
+
+    pub fn load_vm_local(self: *@This(), dst: Reg, src: VMLocal) !void {
+        try self.mov(Operand.init(.reg, dst), Operand.init(.mem_64_base_and_offset, .{
+            .base = Reg.LocalsArrayBase,
+            .offset = @as(u32, @intCast(src * @sizeOf(Value))),
+        }));
+    }
+
+    pub fn increment(self: *@This(), dst: Reg) !void {
+        try self.emit8(0x48);
+        try self.emit8(0xff);
+        try self.emit8(0xc0 | dst.to_underlying());
+    }
+
+    pub fn less_than(self: *@This(), dst: Reg, src: Reg) !void {
+        // cmp src, dst
+        try self.emit8(0x48);
+        try self.emit8(0x39);
+        try self.emit8(0xc0 | (src.to_underlying() << 3) | dst.to_underlying());
+
+        // setl dst
+        try self.emit8(0x0f);
+        try self.emit8(0x9c);
+        try self.emit8(0xc0 | dst.to_underlying());
+
+        // movzx dst, dst
+        try self.emit8(0x48);
+        try self.emit8(0x0f);
+        try self.emit8(0xb6);
+        try self.emit8(0xc0 | (dst.to_underlying() << 3) | dst.to_underlying());
+    }
+
+    pub fn jump(self: *@This(), target: *Block) !void {
+        // jmp target (RIP-relative 32-bit offset)
+        try self.emit8(0xe9);
+        try target.jumps.append(self.output.items.len);
+        try self.emit32(deadbeef);
+    }
+
+    pub fn jump_conditional(self: *@This(), reg: Reg, true_target: *Block, false_target: *Block) !void {
+        // if reg == 0 jump to false_false else jump to true_target
+        // cmp reg, 0
+        try self.emit8(0x48);
+        try self.emit8(0x83);
+        try self.emit8(0xf8);
+        try self.emit8(0x00 | reg.to_underlying());
+
+        // jz false_target (RIP-relative 32-bit offset)
+        try self.emit8(0x0f);
+        try self.emit8(0x84);
+        try false_target.jumps.append(self.output.items.len);
+        try self.emit32(deadbeef);
+
+        // jmp true_target (RIP-relative 32-bit offset)
+        try self.jump(true_target);
+    }
+
+    pub fn exit(self: *@This()) !void {
+        try self.emit8(0xc3);
+    }
+};
+
+const JIT = struct {
+    assembler: Assembler,
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) @This() {
+        return .{
+            .allocator = allocator,
+            .assembler = Assembler.init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.assembler.deinit();
+    }
+
+    pub fn compile(self: *@This(), program: *Program) !Executable {
+        var iter = program.blocks.iterator(0);
+
+        while (iter.next()) |block| {
+            block.offset = self.assembler.output.items.len;
+
+            for (block.instructions.items) |instruction| {
+                switch (instruction) {
+                    .LoadImmediate => |li| try self.compile_load_immediate(li),
+                    .Load => |l| try self.compile_load(l),
+                    .Store => |s| try self.compile_store(s),
+                    .SetLocal => |sl| try self.compile_set_local(sl),
+                    .GetLocal => |gl| try self.compile_get_local(gl),
+                    .Increment => try self.compile_increment(),
+                    .LessThan => |lt| try self.compile_less_than(lt),
+                    .Jump => |jmp| try self.compile_jump(jmp),
+                    .JumpConditional => |jmpc| try self.compile_jump_conditional(jmpc),
+                    .Exit => try self.compile_exit(),
+                }
+            }
+        }
+
+        iter = program.blocks.iterator(0);
+
+        while (iter.next()) |b| {
+            for (b.jumps.items) |j| {
+                const offset: i64 = @as(i64, @intCast(@as(i64, @intCast(b.offset)) - @as(i64, @intCast(j)) - @as(i64, @intCast(4))));
+
+                self.assembler.output.items[j + 0] = @as(u8, @intCast((offset >> @intCast(0)) & 0xff));
+                self.assembler.output.items[j + 1] = @as(u8, @intCast((offset >> @intCast(8)) & 0xff));
+                self.assembler.output.items[j + 2] = @as(u8, @intCast((offset >> @intCast(16)) & 0xff));
+                self.assembler.output.items[j + 3] = @as(u8, @intCast((offset >> @intCast(24)) & 0xff));
+            }
+        }
+
+        try std.fs.cwd().writeFile("output.asm", self.assembler.output.items);
+
+        // TODO: suckma
+        const executable_memory = try std.os.mmap(null, std.mem.alignForward(usize, self.assembler.output.items.len, std.mem.page_size), std.os.PROT.READ | std.os.PROT.WRITE, std.os.MAP.ANONYMOUS | std.os.MAP.PRIVATE, 0, 0);
+        @memcpy(executable_memory[0..self.assembler.output.items.len], self.assembler.output.items);
+
+        try std.os.mprotect(executable_memory, std.os.PROT.READ | std.os.PROT.EXEC);
+
+        return .{
+            .code = executable_memory,
+            .code_size = self.assembler.output.items.len,
+        };
+    }
+
+    pub inline fn compile_load_immediate(self: *@This(), li: Value) !void {
+        try self.assembler.load_immediate64(.GPR0, li);
+        try self.assembler.store_vm_register(0, .GPR0);
+    }
+
+    pub inline fn compile_load(self: *@This(), l: VMRegister) !void {
+        try self.assembler.load_vm_register(.GPR0, l);
+        try self.assembler.store_vm_register(0, .GPR0);
+    }
+
+    pub inline fn compile_store(self: *@This(), s: usize) !void {
+        try self.assembler.load_vm_register(.GPR0, 0);
+        try self.assembler.store_vm_register(s, .GPR0);
+    }
+
+    pub inline fn compile_get_local(self: *@This(), gl: usize) !void {
+        try self.assembler.load_vm_local(.GPR0, gl);
+        try self.assembler.store_vm_register(0, .GPR0);
+    }
+
+    pub inline fn compile_set_local(self: *@This(), sl: usize) !void {
+        try self.assembler.load_vm_register(.GPR0, 0);
+        try self.assembler.store_vm_local(sl, .GPR0);
+    }
+
+    pub inline fn compile_increment(self: *@This()) !void {
+        try self.assembler.load_vm_register(.GPR0, 0);
+        try self.assembler.increment(.GPR0);
+        try self.assembler.store_vm_register(0, .GPR0);
+    }
+
+    pub inline fn compile_less_than(self: *@This(), lt: LessThan) !void {
+        try self.assembler.load_vm_register(.GPR0, lt.lhs);
+        try self.assembler.load_vm_register(.GPR1, 0);
+        try self.assembler.less_than(.GPR0, .GPR1);
+        try self.assembler.store_vm_register(0, .GPR0);
+    }
+
+    pub inline fn compile_jump(self: *@This(), jmp: *Block) !void {
+        try self.assembler.jump(jmp);
+    }
+
+    pub inline fn compile_jump_conditional(self: *@This(), jmpc: JumpConditional) !void {
+        try self.assembler.load_vm_register(.GPR0, 0);
+        try self.assembler.jump_conditional(.GPR0, jmpc.true_target, jmpc.false_target);
+    }
+
+    pub inline fn compile_exit(self: *@This()) !void {
+        try self.assembler.exit();
+    }
+};
